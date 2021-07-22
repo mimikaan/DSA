d04c4224acc951ebbdd3bfb9dfa31a0bb4807063
Pritom Hazarika <mimikaan@gmail.com>
2021-07-22 20:44:50 +0530

    Rest api practice

---
+++ Rest_Api/app.py
@@ -0,0 +1,87 @@
+from flask import Flask
+import pymongo
+from bson.json_util import dumps 
+from bson.objectid import ObjectId
+from flask import jsonify,request 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask (__name__)
+
+app.secret_key = "secretkey"
+
+# app.config['MONGO_URI'] = "mongodb://127.0.0.1:27017/Users"
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+mongo= client['Users']
+
+# mongo = PyMongo(app)
+
+@app.route('/add', methods = ['POST'])
+def add_user():
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+
+    if _name and _email and _password and request.method == 'POST':
+        _hashed_password = generate_password_hash(_password)
+        information  = mongo.Users_details
+
+        id = information.insert_one({'name':_name, 'email':_email,'pwd':_hashed_password})
+        resp = jsonify("User added successfully")
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
+
+@app.route('/users', methods=['GET'])
+def users():
+    users = mongo.Users_details.find()
+    resp = dumps(users)
+    return resp
+
+@app.route('/user/<id>')
+def user(id):
+    user = mongo.Users_details.find_one({'_id':ObjectId(id)})
+    resp = dumps(user)
+    return resp
+
+@app.route('/delete/<id>')
+def delete_user(id):
+    mongo.Users_details.delete_one({'_id':ObjectId(id)})
+    resp = jsonify("User deleted successfullly")
+    resp.status_code = 200
+    return resp
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_user(id):
+    _id = id
+    _json = request.json
+    _name = _json['name']
+    _email = _json['email']
+    _password = _json['pwd']
+    if _name and _email and _password and _id and request.method == 'PUT':
+        _hashed_password = generate_password_hash(_password)
+        mongo.Users_details.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name, 'email':_email,'pwd':_hashed_password}})
+        resp = jsonify('Entries have been updated')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found
+
+
+
+
+@app.errorhandler(404)
+def not_found(error = None):
+    message = {
+        'status':404,
+        'message':'Not Found'+ request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
